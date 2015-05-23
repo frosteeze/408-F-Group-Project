9755ebeb911ed52de0b830ad0f69446cb017ba42
Ethan Ito <ethanitorhs@gmail.com>
2015-05-23 09:56:21 -0700

    Added standalone version of stockquote QuoteStandAlone

---
+++ GroupProject/QuoteStandAlone/QuoteStandAlone/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+open Microsoft.FSharp.Linq
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+open System.Threading
+
+while true do
+    //Loads the json file from the url
+    let doc = JsonValue.Load("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json");
+    //Test to access element
+    //let test = doc?query?count
+    //Console.WriteLine(test)
+    Console.Clear()
+
+    let info = JsonValue.Null
+    (*Match allows us to draw out elements from 
+      the doc element through the JsonValue object 
+      structure. As we only need a single JsonValue 
+      we simply declare a null JsonValue and use that
+    *)
+    match doc with
+    |  info ->
+        // Access count
+        (*The count field is in the query JSON object which 
+        is within  the outermost JSON object represented by 
+        info *) 
+        let count = info?query?count
+        printfn 
+          "Showing %d quotes" 
+          (count.AsInteger())
+        //We will be running asynch in order to limit the number of calls
+        let quotes = info?query?results?quote
+        //We itterate for all records (in this case quotes) that is stored in the quotes field
+        //NOTE: This will not work if we only query for a single quote, we should instead just
+        //manually retrieve the values like how the value of quotes is set
+        for record in quotes do
+            printfn " %-22s %5s  %4.2f " (record?Name.AsString())  (record?symbol.AsString())  (record?LastTradePriceOnly.AsFloat())
+    Thread.Sleep(1000)
